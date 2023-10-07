8dc59623d5e3993fbec9b28ffd1a87b1f247c737
Ojochogwu Dickson <07afrik@gmail.com>
2023-10-06 18:02:25 -0700

    WIP

--- viticket/app/scenes/Nav/index.tsx
+++ viticket/app/scenes/Nav/index.tsx
@@ -3,7 +3,7 @@ import  Menu  from '../../../public/assets/div.svg'
 export default function Navbar(){
     return (
         <nav className="flex justify-between items-center py-6">
-            <div className=" text-yellow-300">VITICKET</div>
+            <div className=" text-[#DC2B46]">VITICKET</div>
             <div className="">
                 <img src={Menu} alt="menu"/>
             </div>
